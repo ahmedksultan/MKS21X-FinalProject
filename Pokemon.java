c0c1fc3d7ea0d2a539c1131cea6bd138593794ab
Ali Taoube <ataoube00@stuy.edu>
2019-01-04 10:17:42 -0500

    Began work on the Pokemon constructor

--- Pokemon.java
+++ Pokemon.java
@@ -1,21 +1,19 @@
-import java.util.*; //scanner
+import java.util.*; //scanner, ArrayList, Map
 import java.io.*; //file, filenotfoundexception
 
 public class Pokemon{
-  public static void main(String[] args) {
-    organizeData();
-  }
+  private String name, type1, type2;
+  private String attack, speed, defense, hp;
+  private ArrayList<String> attacks, typeWeakness, typeResistance;
 
-  private String[][] data;
-
-  public static void organizeData(){
-    String filename = "Pokemon.csv";
-    try{
-      File f = new File(filename);//can combine
-      Scanner in = new Scanner(f);//into one line
-    }
-    catch{
-
+  public Pokemon(String name1){
+    name = name1;
+    for (int x = 0; data[x] < != name; x++){
+      if (data[x] == name){
+        type1 = data[2];
+        type2 = data[3];
+        
+      }
     }
   }
 }

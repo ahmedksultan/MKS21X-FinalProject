a4f440c3aac9a5ff6a8ade0570aaacfbacc1141c
Ali Taoube <ataoube00@stuy.edu>
2019-01-16 20:21:36 -0500

    Implemented three functions in Sprites class, two to initialize array, one to get the ArrayList of Chars.

--- Sprites.java
+++ Sprites.java
@@ -2,24 +2,77 @@ import java.util.*; //scanner, ArrayList, Map
 import java.io.*; //file, filenotfoundexception
 
 public class Sprites{
+  private ArrayList<Character> data;
 
-  public String[][] read(int x){
+
+  public ArrayList<Character> getArray(int x){
     try{
-      File f = new File("Pokemon.csv");
+      File f = new File("Sprites.txt");
       Scanner in = new Scanner(f);
+
       while (in.hasNext()){
         String line = in.nextLine();
-        int y = 0;
+
+        if (line.equals(x+":")){
+          while (!line.equals("break;")){
+            for (int i = 0; x < line.length(); i++){
+              data.add(line.charAt(i));
+            }
+          }
+        }
+    }
+  }
+    catch(FileNotFoundException e){
+      System.out.println("Error at getArray");
+      throw new FileNotFoundException();
+    }
+    return data;
+  }
+
+  public int getNumArrays(int x){
+    try{
+      File f = new File("Sprites.txt");
+      Scanner in = new Scanner(f);
+      int y = 0;
+      while (in.hasNext()){
+        String line = in.nextLine();
+
         if (line.equals(x+":")){
           while (!line.equals("break;")){
             y++;
           }
         }
       }
+      return y;
     }
     catch(FileNotFoundException e){
+      System.out.println("In getNumArrays");
+      throw new Error();
+    }
+    throw new Error();
+  }
 
+  public void getLenArrays(int x){
+
+    try{
+      File f = new File("Sprites.txt");
+      Scanner in = new Scanner(f);
+
+      int y = 0;
+      while (in.hasNext()){
+        String line = in.nextLine();
+
+        if (line.equals(x+":")){
+          while (!line.equals("break;")){
+                    }
+        }
+
+      }
+    }
+    catch(FileNotFoundException e){
+      throw new FileNotFoundException();
     }
   }
+
 
 }

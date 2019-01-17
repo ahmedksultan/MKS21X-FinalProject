07d1805f8817ba5dda27d46da83a1e3c63b67b9f
Ali Taoube <ataoube00@stuy.edu>
2019-01-16 20:30:08 -0500

    Reworked getArray function to prevent an infinite loop because line was remained the same when looping through the Array.

--- Sprites.java
+++ Sprites.java
@@ -2,29 +2,33 @@ import java.util.*; //scanner, ArrayList, Map
 import java.io.*; //file, filenotfoundexception
 
 public class Sprites{
-  private ArrayList<Character> data;
+  public static void main(String[] args) {
+    getArray(150);
+  }
+
+  private static ArrayList<Character> data;
 
 
-  public ArrayList<Character> getArray(int x){
+  public static ArrayList<Character> getArray(int x){
     try{
       File f = new File("Sprites.txt");
       Scanner in = new Scanner(f);
 
       while (in.hasNext()){
         String line = in.nextLine();
-
-        if (line.equals(x+":")){
-          while (!line.equals("break;")){
+        while (!line.equals("break;")){
+          line = in.nextLine();
+          if (line.equals(x+":")){
             for (int i = 0; x < line.length(); i++){
               data.add(line.charAt(i));
             }
           }
         }
+      }
     }
-  }
     catch(FileNotFoundException e){
       System.out.println("Error at getArray");
-      throw new FileNotFoundException();
+      throw new Error();
     }
     return data;
   }
